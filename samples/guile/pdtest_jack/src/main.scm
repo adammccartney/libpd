8cf14db745c1aec82a2340305fb945b397caf557
Adam McCartney <adam.mccartney@tuwien.ac.at>
2024-11-01 22:37:24 +0100

    Minimum viable example
    Note that this requires modifying the libpd.scm file:
    
    >  ;; generated with `guild compile-ffi ffi/libpd.ffi'
    >
    >  (define-module (ffi libpd)
    >    #:use-module (system ffi-help-rt)
    >    #:use-module ((system foreign) #:prefix ffi:)
    >    #:use-module (bytestructures guile))
    >
    >  (define ffi-libpd-llibs
    >    (delay (list (dynamic-link
    >                   (string-append (getenv "GUIX_ENVIRONMENT") "/lib/libpd.so")))))

--- samples/guile/pdtest_jack/src/main.scm
+++ samples/guile/pdtest_jack/src/main.scm
@@ -1,3 +1,8 @@
-(use-modules (ffi libpd))
+(use-modules (system foreign-library)
+             (ffi libpd))
+
+;;(load-foreign-library
+;; (string-append
+;;  (getenv "GUIX_ENVIRONMENT") "/lib/libpd.so"))
 
 (libpd_init)
